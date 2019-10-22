0c97c02b7456c45c6a9a9e3de01d1d6ae576aa21
TheDarkMen3000 <philmoeller10@gmail.com>
2019-10-23 01:53:51 +0200

    Fixed documentation

--- docs/html/classColor.js
+++
@@ -1,9 +0,0 @@
-var classColor =
-[
-    [ "Color", "classColor.html#a154354d16fc63294355979095a3b6d65", null ],
-    [ "Color", "classColor.html#a9277e690210af32026f461305a793778", null ],
-    [ "getBlue", "classColor.html#ab7caa1306b0bdd9c5a49ce640da1a8e8", null ],
-    [ "getColorCodeString", "classColor.html#a300e296b59c4254850e5249c38776522", null ],
-    [ "getGreen", "classColor.html#aa32e85661f7d2844ea20e4ffe2f1fa62", null ],
-    [ "getRed", "classColor.html#aed1e9e09db006f0546c4621ba387efb6", null ]
-];
