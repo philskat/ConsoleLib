0b6a3b3ee245afa96468fbad4665606c627f8a3f
TheDarkMen3000 <philmoeller10@gmail.com>
2019-10-23 18:42:28 +0000

    New docs from commit 7a31067b02198363d90991353885c9579f300bed

--- docs/classcon_1_1Color.js
+++
@@ -1,9 +0,0 @@
-var classcon_1_1Color =
-[
-    [ "Color", "classcon_1_1Color.html#adf463360c1d603353e4c8be12e17c3ca", null ],
-    [ "Color", "classcon_1_1Color.html#aa2768b6635559442db2a0e46f7ba0906", null ],
-    [ "getBlue", "classcon_1_1Color.html#a157a0b4661acaeed605b00a559be920a", null ],
-    [ "getColorCodeString", "classcon_1_1Color.html#a5cc0ae893390c9f5fad58c44cb945994", null ],
-    [ "getGreen", "classcon_1_1Color.html#aae2304cb32a66d35e239953aea6e6788", null ],
-    [ "getRed", "classcon_1_1Color.html#a05dfef33a01618a44be8e5088939a9f1", null ]
-];
